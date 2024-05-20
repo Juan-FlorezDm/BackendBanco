6631c9877d1127208f67c068302395bc0d10f270
Juan-FlorezDm <jnendez38@gmail.com>
2024-05-20 08:31:52 -0500

    quick update

--- src/Usuario.java
+++ src/Usuario.java
@@ -3,7 +3,4 @@ public class Usuario{
     public static void IngresarUsuario(String Nombre, int Cedula, int NumeroContacto){
         ConexionBaseDatos.IngresarUser(Nombre, Cedula, NumeroContacto);
     }
-    public static void main(String[] args) {
-        IngresarUsuario("Rodriguez", 1251315, 305);
-    }
 }
